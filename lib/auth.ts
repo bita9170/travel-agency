d6f07c6c1ca2cc623abbcdf34563afb5f36a85d1
bita9170 <bita.s9170@gmail.com>
2024-04-25 16:08:17 +0200

    [#12] Convert javascript to typescript in nextjs project

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,17 @@
-// lib/auth.js
+// lib/auth.ts
 import jwt from "jsonwebtoken";
 
-const SECRET_KEY = process.env.JWT_SECRET;
+import { NextRequest } from "next/server";
 
-export const authenticateToken = async (req) => {
+const SECRET_KEY: any = process.env.JWT_SECRET;
+
+export const authenticateToken = async (req: NextRequest) => {
   const authHeader = req.headers.get("authorization");
   const token = authHeader && authHeader.split(" ")[1];
   if (token == null) throw new Error("No token provided");
 
   return new Promise((resolve, reject) => {
-    jwt.verify(token, SECRET_KEY, (err, user) => {
+    jwt.verify(token, SECRET_KEY, (err: any, user: any) => {
       if (err) reject(new Error("Invalid token"));
       resolve(user);
     });
