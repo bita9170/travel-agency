14315507843cf46aa10eb084cafe3b18c36c49d0
bita9170 <bita.s9170@gmail.com>
2024-04-25 21:40:17 +0200

    #7 integration-tripadvisor-api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Register from "@/components/Register";
+import { Search } from "@/controllers/tripadvisorController";
 
-export default function Home() {
+export default async function Home() {
+  const data = await Search("Berlin", "en");
+
+  console.log(data);
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
