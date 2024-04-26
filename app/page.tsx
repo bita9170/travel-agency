66301163181e5e4302ee5625dd232d985aab9201
oscar <oscar.vanvelsen@gmail.com>
2024-04-26 11:24:44 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { searchAllLocations } from "@/controllers/tripadvisorController";
 export default async function Home() {
   const data = await searchAllLocations("Berlin", "en");
 
-  console.log(data);
+  // console.log(data);
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
