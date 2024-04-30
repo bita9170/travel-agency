91f0d71d8d7491afb87c0e47d76ce2f8e10a0714
bita9170 <bita.s9170@gmail.com>
2024-04-30 12:14:51 +0200

    [#22] Add avatar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,26 @@
 import { Button } from "@/components/ui/button";
 import Register from "@/components/Register";
-import { searchAllLocations } from "@/controllers/tripadvisorController";
+import {
+  searchAllLocations,
+  getLocationReviews,
+  searchHotels,
+} from "@/controllers/tripadvisorController";
+import Logo from "@/components/elements/Logo";
+import Avatar from "@/components/elements/Avatar";
 
 export default async function Home() {
-  const data = await searchAllLocations("Berlin", "en");
+  const data = await searchHotels("Hilton");
 
-  // console.log(data);
+  console.log(data);
   return (
     <main>
-      <h1 className="text-3xl border border-[rgb(0,0,0)] font-bold underline">
+      {/* <h1 className="text-3xl border border-[rgb(0,0,0)] font-bold underline">
         Hello world!
       </h1>
       <Button>Click me</Button>
-      <Register />
+      <Register /> */}
+      <Logo />
+      <Avatar src="/avatar.jpeg" alt="avatar" fallBack="BS" />
     </main>
   );
 }
