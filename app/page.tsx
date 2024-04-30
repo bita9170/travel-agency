d1dce10535e6cc385a36d92cebc77d76d5a1a868
bita9170 <bita.s9170@gmail.com>
2024-04-30 12:32:22 +0200

    fixed packege.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Register from "@/components/Register";
-import {
-  searchAllLocations,
-  getLocationReviews,
-  searchHotels,
-} from "@/controllers/tripadvisorController";
+
 import Logo from "@/components/elements/Logo";
 import Avatar from "@/components/elements/Avatar";
 
 export default async function Home() {
-  const data = await searchHotels("Hilton");
-
-  console.log(data);
+ 
   return (
     <main>
       {/* <h1 className="text-3xl border border-[rgb(0,0,0)] font-bold underline">
