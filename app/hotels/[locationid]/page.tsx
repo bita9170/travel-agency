48736ebcc328372155e311fb462d58691e235a1e
friesenkatharina <friesen.katharina2022@gmail.com>
2024-05-14 15:08:08 +0200

    Refactor location details page and add location card component

--- app/hotels/[locationid]/page.tsx
+++ app/hotels/[locationid]/page.tsx
@@ -1,21 +1,70 @@
-import MaxLimitWrapper from "@/components/elements/MaxLimitWrapper";
-import { getLocationDetails } from "@/controllers/tripadvisorController";
-import { LocationDetails } from "@/lib/class/location";
 import React from "react";
+import MaxLimitWrapper from "@/components/elements/MaxLimitWrapper";
+// import { getLocationDetails } from "@/controllers/tripadvisorController";
+// import { LocationDetails } from "@/lib/class/location";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
 
-async function page({ params }: any) {
-  const { locationid } = params;
+const LocationDetails = [
+  //static Data
 
-  const locations: LocationDetails[] = await getLocationDetails(locationid);
-  const location = locations[0];
+  {
+    title: "Eiffelturm",
+    reviews: "142,687 Bewertungen",
+    categories:
+      "Sehenswürdigkeiten & Wahrzeichen • Aussichtsplattformen & -türme",
+    hours: "Jetzt geöffnet • 9:00 AM - 11:45 PM",
+    about:
+      "Der Eiffelturm, ein Wahrzeichen der Architektur und Ingenieurskunst, zieht jährlich Millionen Besucher an. Als Symbol von Paris bietet der Turm atemberaubende Ausblicke auf die Stadt. Der Besuch kann mehr als drei Stunden dauern und umfasst Optionen für Führungen sowie den Zugang zu verschiedenen Ebenen des Turms.",
+    duration: "Mehr als 3 Stunden",
+    image: "/public/image1.jpeg",
+  },
+];
 
+// async function page({ params }: any) {
+//   //   const { locationid } = params;
+//   //   const locations: LocationDetails[] = await getLocationDetails(locationid);
+//   //   const location = locations[0];
+
+//   return (
+//     <MaxLimitWrapper>
+//       {/* <h3>{location.getName()}</h3>
+//       <img src={(await location.getPhotos())[0].getLarge().url} />
+//       <div> {location.getDescription()}</div> */}
+//     </MaxLimitWrapper>
+//   );
+// }
+
+export function LocationDetailsCard() {
   return (
-    <MaxLimitWrapper>
-      <h3>{location.getName()}</h3>
-      <img src={(await location.getPhotos())[0].getLarge().url} />
-      <div> {location.getDescription()}</div>
-    </MaxLimitWrapper>
+    <Card className="w-[350px] shadow-lg">
+      <MaxLimitWrapper>
+        <CardHeader>
+          <Image
+            src={LocationDetails[0].image}
+            alt="Eiffelturm"
+            layout="fill"
+            objectFit="cover"
+          />
+          <CardTitle>{LocationDetails[0].title}</CardTitle>
+          <CardDescription>{LocationDetails[0].reviews}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>{LocationDetails[0].categories}</p>
+          <p>{LocationDetails[0].hours}</p>
+          <p>{LocationDetails[0].about}</p>
+        </CardContent>
+        <CardFooter>
+          <p>Dauer: {LocationDetails[0].duration}</p>
+        </CardFooter>
+      </MaxLimitWrapper>
+    </Card>
   );
 }
-
-export default page;
