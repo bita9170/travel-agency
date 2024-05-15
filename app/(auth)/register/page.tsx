1ac027a3ccd0af063905b31e156361df2f4780c4
Bita Sadeghi <bita.s9170@gmail.com>
2024-05-15 11:21:41 +0200

    Added router of user actions

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Register</div>;
+}
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Dashboard</div>;
+}
+
+export default page;
