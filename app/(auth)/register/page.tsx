a4b704cbe545b45a33ab3ffebd20be43b3bf784e
friesenkatharina <friesen.katharina2022@gmail.com>
2024-05-16 14:01:55 +0200

    Add next-auth package to package.json

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Logo from "@/components/elements/Logo";
 import { Button } from "@/components/ui/button";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,15 +8,14 @@ import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import MaxLimitWrapper from "@/components/elements/MaxLimitWrapper";
 
 const formSchema = z.object({
   fullName: z.string().min(2).max(50),
@@ -32,19 +30,7 @@ const formSchema = z.object({
   }),
 });
 
-const styles = {
-  text: {
-    fontSize: "16px",
-    color: "black",
-  },
-};
-
-const defaultProps = {
-  text: "EUR",
-  color: "black",
-};
-
-function Register(props: any) {
+function Register() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -96,191 +82,174 @@ function Register(props: any) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-2 mt-10">
-      <div className="py-4">
-        <h1 className="text-2xl font-bold text-[48px]">Register</h1>
-        <p className="text-center text-[14px] mt-4 text-muted-foreground">
-          Join us to start planning your next adventure
-        </p>
-      </div>
-
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 ">
-          <div className="form mt-10">
-            <FormField
-              control={form.control}
-              name="fullName"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center font-family ">
-                  <FormLabel className="text-[14px] pl-1">Full Name</FormLabel>
-
-                  <Input
-                    style={{ marginBottom: "10px" }}
-                    placeholder="Full Name"
-                    {...field}
-                    className="w-[468px] h-[48px] p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none "
-                  />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="birthday"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center">
-                  <FormLabel className="text-[14px] pl-1 ">Birthday</FormLabel>
-
-                  <Input
-                    placeholder="Birthday"
-                    {...field}
-                    className="w-[468px] h-[48px]  p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none "
-                  />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center">
-                  <FormLabel className="text-[14px] pl-1">Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Email"
-                      {...field}
-                      className="w-[468px] h-[48px] p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center">
-                  <FormLabel className="text-[14px] pl-1">Username</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Username"
-                      {...field}
-                      className="w-[468px] h-[48px] p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="address"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center">
-                  <FormLabel className="text-[14px] pl-1">Address</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Address"
-                      {...field}
-                      className="w-[468px] h-[48px] p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center">
-                  <FormLabel className="text-[14px] pl-1">
-                    Set up a secure password
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Password"
-                      type="password"
-                      {...field}
-                      className="w-[468px] h-[48px] p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem className="w-[468px] align-items-center">
-                  <FormLabel className="text-[14px] pl-1">
-                    Confirm Password
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Confirm Password"
-                      type="password"
-                      {...field}
-                      className="w-[468px] h-[48px] p-2 border-0 box-border rounded-[22px] shadow-md bg-white text-black text-[14px] font-fira-sans font-medium leading-[18px] outline-none"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <FormField
-            control={form.control}
-            name="agreeToTerms"
-            render={({ field }) => (
-              <FormItem className="w-[468px] flex items-center ">
-                <input
-                  type="checkbox"
-                  {...field}
-                  className="mr-4 mt-2 border-none"
-                />
-                <FormLabel className="text-center">
-                  By signing up, you agree to our
-                  <Link href="/terms" className="text-black">
-                    Terms
-                  </Link>
-                  and
-                  <Link href="/privacy" className="text-black">
-                    Privacy Policy.
-                  </Link>
-                </FormLabel>
-              </FormItem>
-            )}
-          />
-          <FormMessage />
-          <Button
-            type="submit"
-            className="
-              w-[468px]
-              h-[48px]
-              px-2
-              border-0
-              box-border
-              rounded-[22px]
-              bg-[#34e0a1]
-              text-black
-              text-[16px]
-              font-fira-sans
-              font-bold
-              leading-[22px]
-              outline-none
-              cursor-pointer
-              "
-          >
-            Join
-          </Button>
-          <p className="text-center mt-4 text-muted-foreground">
-            Already a member?
-            <Link href="/login" className="text-black">
-              Log in
-            </Link>
+    <MaxLimitWrapper>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-center space-y-2">
+          <h2 className="text-3xl font-bold">Register</h2>
+          <p className="text-muted-foreground">
+            Join us to start planning your next adventure
           </p>
-        </form>
-      </Form>
-    </div>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="form mt-6 max-w-md mx-auto space-y-4">
+              <FormField
+                control={form.control}
+                name="fullName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">Full Name</FormLabel>
+
+                    <Input
+                      placeholder="Full Name"
+                      {...field}
+                      className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                    />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="birthday"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">Birthday</FormLabel>
+
+                    <Input
+                      placeholder="day.month.year"
+                      {...field}
+                      className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                    />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Email"
+                        {...field}
+                        className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Username"
+                        {...field}
+                        className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Musterstrasse 1, Musterstadt"
+                        {...field}
+                        className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">
+                      Set up a secure password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        type="password"
+                        {...field}
+                        className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="ml-2">Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Confirm Password"
+                        type="password"
+                        {...field}
+                        className="p-4 shadow-shadowSmall border-2 rounded-3xl"
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="agreeToTerms"
+                render={({ field }) => (
+                  <FormItem className="items-top flex space-x-2">
+                    <input
+                      type="checkbox"
+                      {...field}
+                      id={field.name}
+                      className="self-start mt-2 ml-2"
+                    />
+                    <div className="grid gap-1.5 leading-none">
+                      <FormLabel
+                        htmlFor={field.name}
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                      >
+                        Accept terms and conditions
+                      </FormLabel>
+                      <p className="text-sm text-muted-foreground">
+                        You agree to our Terms of Service and Privacy Policy.
+                      </p>
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormMessage />
+              <Button variant={"green"} className="w-full">
+                Join
+              </Button>
+              <p className="text-center text-muted-foreground">
+                Already a member
+                <Link href="/login" className="ml-1 text-black">
+                  Log in
+                </Link>
+              </p>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </MaxLimitWrapper>
   );
 }
 
