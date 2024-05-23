dee233ebd6edd812dc592f8decfbdd64b5f759b4
Bita Sadeghi <126606391+bita9170@users.noreply.github.com>
2024-05-23 09:47:59 +0000

    Fixed build

--- components/tabsection/Tab.tsx
+++ components/tabsection/Tab.tsx
@@ -10,8 +10,8 @@ function Tab({ tabsHeading, data }: { tabsHeading: string; data: TabProps[] }) {
   return (
     <div className="tab-section">
       <h3>{tabsHeading}</h3>
-      <Tabs defaultValue={data[0].title}>
-        <TabsList className="md:flex gap-6">
+      <Tabs defaultValue={data[0].title} className="overflow-auto whitespace-nowrap">
+        <TabsList className="md:flex gap-6 navigation">
           {data?.map((item, index) => (
             <TabsTrigger key={index} value={item.title}>
               {item.title}
